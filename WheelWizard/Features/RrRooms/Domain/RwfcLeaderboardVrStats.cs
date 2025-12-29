e074ff8e839e15e635355d6d12e0b871beb17787
Patchzy <64382339+patchzyy@users.noreply.github.com>
2025-12-29 22:58:16 +0100

    Update RwfcLeaderboardVrStats.cs

--- WheelWizard/Features/RrRooms/Domain/RwfcLeaderboardVrStats.cs
+++ WheelWizard/Features/RrRooms/Domain/RwfcLeaderboardVrStats.cs
@@ -2,7 +2,7 @@ namespace WheelWizard.RrRooms;
 
 public sealed class RwfcLeaderboardVrStats
 {
-    public int? Last24Hours { get; set; }
-    public int? LastWeek { get; set; }
-    public int? LastMonth { get; set; }
+    public int Last24Hours { get; set; } = 0;
+    public int LastWeek { get; set; } = 0;
+    public int LastMonth { get; set; } = 0;
 }
