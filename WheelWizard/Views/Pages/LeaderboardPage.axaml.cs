bf1eb9ee2bb3884940062c28c4c609d81372ee17
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2026-02-22 21:24:14 +0100

    combine leaderboard Player Items

--- WheelWizard/Views/Pages/LeaderboardPage.axaml.cs
+++ WheelWizard/Views/Pages/LeaderboardPage.axaml.cs
@@ -20,6 +20,30 @@ using WheelWizard.WiiManagement.MiiManagement.Domain.Mii;
 
 namespace WheelWizard.Views.Pages;
 
+public sealed record LeaderboardPlayerItem
+{
+    public required int Rank { get; init; }
+    public required string PlacementLabel { get; init; }
+    public required string Name { get; init; }
+    public required string FriendCode { get; init; }
+    public required string VrText { get; init; }
+    public Mii? Mii { get; init; }
+    public BadgeVariant PrimaryBadge { get; init; }
+    public bool HasBadge { get; init; }
+    public bool IsSuspicious { get; init; }
+    public bool IsEvenRow { get; init; }
+    public bool IsFriend { get; init; }
+    public bool IsOnline { get; init; }
+
+    // Keep parity with RoomDetailsPage player template bindings.
+    public string VrDisplay => VrText;
+    public Mii? FirstMii => Mii;
+    public bool HasBadges => HasBadge;
+    public bool IsTopLeaderboardPlayer => true;
+    public string TopLabel => $"#{Rank}";
+    public bool IsOpenHost => false;
+}
+
 public partial class LeaderboardPage : UserControlBase, INotifyPropertyChanged
 {
     private static readonly LeaderboardPlayerItem EmptyPodiumPlayer = new()
