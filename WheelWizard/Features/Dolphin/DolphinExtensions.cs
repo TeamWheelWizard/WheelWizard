0d2d7f8b5bf74f075b96346c816a71f5c6472af7
patchzyy <64382339+patchzyy@users.noreply.github.com>
2026-09-26 09:34:46 +0200

    Inject Dolphin discovery

---
+++ WheelWizard/Features/Dolphin/DolphinExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using WheelWizard.Dolphin.Discovery;
+using WheelWizard.Dolphin.Paths;
+using WheelWizard.Shared.Platform;
+
+namespace WheelWizard.Dolphin;
+
+public static class DolphinExtensions
+{
+    public static IServiceCollection AddDolphin(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IRuntimeEnvironment, RuntimeEnvironment>();
+        services.TryAddSingleton<IDolphinPathResolver, DolphinPathResolver>();
+        services.TryAddSingleton<IDolphinRegistrySettings, DolphinRegistrySettings>();
+        services.TryAddSingleton<IDolphinDiscoveryService, DolphinDiscoveryService>();
+        return services;
+    }
+}
--- WheelWizard/Features/Settings/SettingsExtensions.cs
+++ WheelWizard/Features/Settings/SettingsExtensions.cs
@@ -1,6 +1,4 @@
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using WheelWizard.Dolphin.Paths;
-using WheelWizard.Shared.Platform;
+using WheelWizard.Dolphin;
 
 namespace WheelWizard.Settings;
 
@@ -15,8 +13,7 @@ public static class SettingsExtensions
 
         // TODO:  Investigate / migrate to IOptions: https://learn.microsoft.com/en-us/dotnet/core/extensions/options
 
-        services.TryAddSingleton<IRuntimeEnvironment, RuntimeEnvironment>();
-        services.TryAddSingleton<IDolphinPathResolver, DolphinPathResolver>();
+        services.AddDolphin();
         services.AddSingleton<ISettingsSignalBus, SettingsSignalBus>();
         services.AddSingleton<IWhWzSettingManager, WhWzSettingManager>();
         services.AddSingleton<IDolphinSettingManager, DolphinSettingManager>();
